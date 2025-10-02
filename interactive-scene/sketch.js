8fe727a8b6cf2ae6f23a05e9fee2bd5388006c64
222gremlins <5300207@spsd.sk.ca>
2025-10-02 09:45:59 -0600

    add new code

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -5,13 +5,3 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-
-  circle(mouseX, mouseY, 50);
-}
