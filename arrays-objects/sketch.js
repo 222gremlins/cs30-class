e8959bee311a9ce6662712da820fea414c3bad75
222gremlins <5300207@spsd.sk.ca>
2025-10-10 09:23:53 -0600

    added my initial thoughts on new assignment

---
+++ arrays-objects/sketch.js
@@ -0,0 +1,22 @@
+// Memory Card Game
+// Elaine King
+// 10/26/2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+//memory card game in progress huzzah.
+
+//card... store: x, y, image, type of card?
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(220);
+
+}
+
