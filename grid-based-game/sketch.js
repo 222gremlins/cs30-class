4dfd8baf4f11b6e9b77c5f8b8246fce4475cfaa5
222gremlins <5300207@spsd.sk.ca>
2025-10-30 09:47:58 -0600

    demo on how to save json into project and then created the file for the new assignment

---
+++ grid-based-game/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Elaine King
+// 10/30/2025
+//
+// Extra for Experts:
+// - 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(220);
+  
+}
